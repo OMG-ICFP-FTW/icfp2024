bdb38c85134e45af066ac068ffac4770e237577e
A K <alex.kesling@gmail.com>
2024-06-28 12:28:42 -0400

    Fix service send() to use auth header file and decode response correctly

--- icfp_lang/service.py
+++ icfp_lang/service.py
@@ -1,20 +1,22 @@
 import requests
 
-from icfp_lang import strings
 from icfp_lang import language
 
 competition_server = 'https://boundvariable.space/communicate'
 
-def message(msg: str) -> Program:
-    send(language.Program(language.String(msg)))
+def message(msg: str) -> language.Program:
+    return send(language.Program(language.String(msg)))
 
-def send(program: Program) -> Program:
+
+def send(program: language.Program) -> language.Program:
     program_message = language.encode(program)
     auth_header = read_auth_header()
     (key, val) = auth_header.split(': ')
-    response = requests.post(competition_server, data=program_message, headers={key: val})
-    return language.decode(response.content)
+    response = requests.post(
+        competition_server, data=program_message, headers={key: val})
+    return language.decode(str(response.content, 'utf-8'))
+
 
 def read_auth_header() -> str:
     with open('misc/SUBMISSION_HEADER.txt') as auth_header:
-        return auth_header.readlines()[0]
+        return auth_header.readlines()[0].strip()
