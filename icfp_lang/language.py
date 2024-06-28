5c3aef89bea718ef6b72d8e0674635a312f6683f
A K <alex.kesling@gmail.com>
2024-06-28 12:37:47 -0400

    Write a simple interactive terminal

--- icfp_lang/language.py
+++ icfp_lang/language.py
@@ -26,3 +26,10 @@ def encode(pgm: Program) -> str:
 def decode(string: str) -> Program:
     """Serialize a program to the ICFP encoding"""
     return Program(String(strings.decode(string)))
+
+
+def human_readable(pgm: Program) -> str:
+    if pgm.val and isinstance(pgm.val, String):
+        return pgm.val.val
+    else:
+        raise ValueError('ICFP Program structure not recognized')
--- icfp_lang/terminal.py
+++ icfp_lang/terminal.py
@@ -1,3 +1,8 @@
-from icfp_lang import service
+from icfp_lang import service, language
 
-
+def start():
+    command = input("> ")
+    while command not in ('end', 'bye', 'stop'):
+        print(language.human_readable(service.message(command)))
+        command = input("> ")
+    print("Goodbye!")
