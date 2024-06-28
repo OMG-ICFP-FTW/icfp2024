311530613150a2a703e08889c9b86e1a19cbb5de
A K <alex.kesling@gmail.com>
2024-06-28 12:17:41 -0400

    Add some more AST bits and bobbins and start implementing send

--- icfp_lang/language.py
+++ icfp_lang/language.py
@@ -1,9 +1,28 @@
+from typing import Union
+
 from dataclasses import dataclass
+
+from icfp_lang import strings
 
 @dataclass
 class String:
     val: str
 
+
 @dataclass
 class Program:
     """A program in Interstellar Communication Functional Program (ICFP)"""
+    val: Union[String]
+
+
+def encode(pgm: Program) -> str:
+    """Serialize a program to the ICFP encoding"""
+    if pgm.val and isinstance(pgm.val, String):
+        return strings.encode(pgm.val.val)
+    else:
+        raise ValueError('ICFP Program structure not recognized')
+
+
+def decode(string: str) -> Program:
+    """Serialize a program to the ICFP encoding"""
+    return Program(String(strings.decode(string)))
--- icfp_lang/service.py
+++ icfp_lang/service.py
@@ -6,8 +6,15 @@ from icfp_lang import language
 competition_server = 'https://boundvariable.space/communicate'
 
 def message(msg: str) -> Program:
-    send(language.String(msg))
+    send(language.Program(language.String(msg)))
 
 def send(program: Program) -> Program:
-    program_message = language.serialize(program)
-    requests.post(competition_server, headers={"Content-Type":"text"})
+    program_message = language.encode(program)
+    auth_header = read_auth_header()
+    (key, val) = auth_header.split(': ')
+    response = requests.post(competition_server, data=program_message, headers={key: val})
+    return language.decode(response.content)
+
+def read_auth_header() -> str:
+    with open('misc/SUBMISSION_HEADER.txt') as auth_header:
+        return auth_header.readlines()[0]
