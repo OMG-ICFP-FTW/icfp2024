93134016e48947c1ace512232c34240260e23d98
A K <alex.kesling@gmail.com>
2024-06-28 22:54:16 -0400

    Parse tree to JSON

--- rust_parser/src/bin/rust_parser.rs
+++ rust_parser/src/bin/rust_parser.rs
@@ -1,7 +1,26 @@
+use std::fs;
+use std::path;
+
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+struct Args {
+    #[arg(long)]
+    input: path::PathBuf,
+    // #[arg(long)]
+    // output: path::PathBuf,
+}
+
 #[derive(pest_derive::Parser)]
 #[grammar = "../icfp.pest"]
-struct _ICFPParser;
+struct ICFPParser;
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let input_icfp = fs::read_to_string(&args.input).unwrap();
+    let parse_result = <ICFPParser as pest::Parser<_>>::parse(Rule::expr, &input_icfp).unwrap();
+
+    println!("{}", serde_json::to_string_pretty(&parse_result).unwrap());
 }
