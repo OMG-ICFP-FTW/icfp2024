b63985b598b6c58edb641c0b2ca1f8f289444d0c
A K <alex.kesling@gmail.com>
2024-06-29 11:11:30 -0400

    Finish first pass of recursive parser... but it needs tests

--- rust_parser/src/bin/rust_parser.rs
+++ rust_parser/src/bin/rust_parser.rs
@@ -20,12 +20,32 @@ struct ICFPParser;
 
 fn parse(parse_tree: pest::iterators::Pair<Rule>) -> anyhow::Result<Expr> {
     match parse_tree.as_rule() {
-        Rule::string => Ok(Expr::Value(Value::decode(
+        Rule::string => Ok(Expr::Value(Value::decode_string(
             parse_tree.into_inner().next().unwrap().as_str(),
         ))),
-        Rule::integer => todo!("integer"),
-        Rule::boolean => todo!("boolean"),
-        Rule::unary => todo!("unary"),
+        Rule::integer => {
+            let mut inner = parse_tree.into_inner();
+            Ok(Expr::Value(Value::decode_integer(
+                inner.next().unwrap().as_str(),
+            )))
+        }
+        Rule::boolean => {
+            let mut inner = parse_tree.into_inner();
+            Ok(Expr::Value(Value::Bool(
+                inner.next().unwrap().as_str() == "T",
+            )))
+        }
+        Rule::unary => {
+            let mut inner = parse_tree.into_inner();
+            if let Expr::Value(value) = parse(inner.next().unwrap())? {
+                Ok(Expr::Unary(Unary {
+                    op: UnaryOp::from_str(inner.next().unwrap().as_str()),
+                    val: value,
+                }))
+            } else {
+                anyhow::bail!("Unary inner parsed to non-value type.")
+            }
+        }
         Rule::binary => {
             let mut inner = parse_tree.into_inner();
             Ok(Expr::Binary(Binary {
@@ -34,10 +54,27 @@ fn parse(parse_tree: pest::iterators::Pair<Rule>) -> anyhow::Result<Expr> {
                 second: Box::new(parse(inner.next().unwrap())?),
             }))
         }
-        Rule::r#if => todo!(),
-        Rule::lambda => todo!(),
-        Rule::variable => todo!(),
-        Rule::expr => todo!(),
+        Rule::r#if => {
+            let mut inner = parse_tree.into_inner();
+            Ok(Expr::If(If {
+                condition: Box::new(parse(inner.next().unwrap())?),
+                if_true: Box::new(parse(inner.next().unwrap())?),
+                if_false: Box::new(parse(inner.next().unwrap())?),
+            }))
+        }
+        Rule::lambda => {
+            let mut inner = parse_tree.into_inner();
+            Ok(Expr::Lambda(Lambda {
+                body: Value::decode_integer_body(inner.next().unwrap().as_str()),
+                arg: Box::new(parse(inner.next().unwrap())?),
+            }))
+        }
+        Rule::variable => {
+            let mut inner = parse_tree.into_inner();
+            Ok(Expr::Variable(Variable(Value::decode_integer_body(
+                inner.next().unwrap().as_str(),
+            ))))
+        }
         _ => unimplemented!(),
     }
 }
@@ -49,6 +86,6 @@ fn main() {
     let mut parse_result = <ICFPParser as pest::Parser<_>>::parse(Rule::expr, &input_icfp).unwrap();
 
     let parse_tree = parse_result.next().unwrap();
-    let ast = parse(parse_tree);
-    println!("AST: {:?}", ast);
+    let ast = parse(parse_tree).unwrap();
+    println!("AST: {}", serde_json::to_string_pretty(&ast).unwrap());
 }
--- rust_parser/src/lib.rs
+++ rust_parser/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -7,9 +9,62 @@ pub enum Value {
     Int(i64),
 }
 
+static TARGET: &'static str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
 impl Value {
-    pub fn decode(encoded: &str) -> Value {
-        todo!("Decode string: {}", encoded)
+    pub fn decode_string(encoded: &str) -> Value {
+        // TODO(alex): wrap in lazy_static
+        let decode_translation_table: HashMap<char, char> = TARGET
+            .chars()
+            .zip((33..(33 + TARGET.len() as u32)).map(|c| c as u8 as char))
+            .collect();
+        Value::Str(
+            encoded
+                .chars()
+                .map(|c| *decode_translation_table.get(&c).unwrap_or(&c))
+                .collect(),
+        )
+    }
+
+    pub fn decode_integer(encoded: &str) -> Value {
+        Value::Int(Value::decode_integer_body(encoded))
+    }
+
+    pub fn decode_integer_body(encoded: &str) -> i64 {
+        // TODO(alex): wrap in lazy_static
+        // Define the base 94 characters
+        let base94_chars: Vec<char> = (33..127).map(|c| c as u8 as char).collect();
+        // Create a reverse lookup map from characters to their indices (values)
+        let char_to_value: std::collections::HashMap<char, i64> = base94_chars
+            .iter()
+            .enumerate()
+            .map(|(idx, &ch)| (ch, idx as i64))
+            .collect();
+
+        let mut result = 0_i64;
+        let mut power = 1_i64;
+        for ch in encoded.chars().rev() {
+            if let Some(&value) = char_to_value.get(&ch) {
+                result += value * power;
+                power *= 94;
+            } else {
+                panic!("character in base-94 encoding unrecognized when decoding integer body ({}): '{}'", encoded, ch);
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod decode_test {
+    use super::*;
+
+    #[test]
+    fn string() {}
+
+    #[test]
+    fn integer() {
+        assert_eq!(1337_i64, Value::decode_integer_body("/6"));
     }
 }
 
@@ -19,6 +74,18 @@ pub enum UnaryOp {
     Not,
     StrToInt,
     IntToStr,
+}
+
+impl UnaryOp {
+    pub fn from_str(source: &str) -> UnaryOp {
+        match source {
+            "-" => UnaryOp::Neg,
+            "!" => UnaryOp::Not,
+            "#" => UnaryOp::StrToInt,
+            "$" => UnaryOp::IntToStr,
+            _ => unimplemented!(),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -127,7 +194,7 @@ pub struct Lambda {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Variable(i64);
+pub struct Variable(pub i64);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Expr {
