ce0afb8f41a8457718496b6a6d35d7e6297cb531
A K <alex.kesling@gmail.com>
2024-06-28 23:05:59 -0400

    Adjust service to allow raw responses and pull down more lambdaman probs

--- icfp_lang/service.py
+++ icfp_lang/service.py
@@ -5,16 +5,16 @@ from icfp_lang import language
 competition_server = 'https://boundvariable.space/communicate'
 
 def message(msg: str) -> language.Program:
-    return send(language.Program(language.Value(msg)))
+    return language.decode(send(language.Program(language.Value(msg))))
 
 
-def send(program: language.Program) -> language.Program:
+def send(program: language.Program) -> str:
     program_message = language.encode(program)
     auth_header = read_auth_header()
     (key, val) = auth_header.split(': ')
     response = requests.post(
         competition_server, data=program_message, headers={key: val})
-    return language.decode(str(response.content, 'utf-8'))
+    return str(response.content, 'utf-8')
 
 
 def read_auth_header() -> str:
