2728302bd0d13f61d1a8b5bab51945f7b1a8f22b
A <machinaut@github>
2024-06-30 19:10:18 -0700

    download lambdaman levels

---
+++ problems/lambdaman/download.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# %%
+import requests
+import os
+import json
+import time
+from dataclasses import dataclass, field
+
+
+post_addr = "https://boundvariable.space/communicate"
+# Authorization header
+auth_path = '../../misc/SUBMISSION_HEADER.txt'
+with open(auth_path, 'r') as file:
+    auth = file.read()
+assert auth.startswith("Authorization: Bearer ")
+# convert to dict for requests
+auth = {"Authorization": auth.lstrip("Authorization: ").strip()}
+
+str_reference = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
+decode_map = {chr(k): v for (k, v) in zip(list(range(33,33 + len(str_reference))),str_reference)}
+encode_map = {v: k for (k, v) in decode_map.items()}
+encode_trans = str.maketrans(encode_map)
+
+
+def request(s):
+    data = 'S' + s.translate(encode_trans)
+    response = requests.post(post_addr, headers=auth, data=data)
+    response.raise_for_status()
+    return response.text
+
+
+for i in range(1, 22):
+    print(f"level{i}")
+    with open(f"level{i}.icfp", 'w') as f:
+        f.write(request(f"get lambdaman{i}"))
+    time.sleep(5)
